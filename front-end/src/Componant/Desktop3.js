5818fbf01fe8df7c088361a57c02e2dbd9a0a754
huynguyen-jo-jo <73032228+huynguyen-jo-jo@users.noreply.github.com>
2021-12-09 11:05:44 +0700

    add Desktop 3

--- front-end/src/Componant/Desktop3.js
+++ front-end/src/Componant/Desktop3.js
@@ -5,16 +5,37 @@ function Desktop3() {
     return (
         <div style ={{width: '80%', margin: 'auto'}}>
           <Container fluid="sm">
-          <Row xl={4} xs={1} md={3} className="g-4" style={{marginTop:'20px' }}>
+          <Row 
+            xl={4} 
+            xs={1} 
+            md={3}
+            className="g-4" 
+            style={{marginTop:'20px' }}>
   {Array.from({ length: 16 }).map((_, idx) => (
     <Col>
       <Card style={{marginTop:'20px' }}>
-        <Card.Img variant="top" src="https://haymora.com/blog/wp-content/uploads/2021/11/nhung-dieu-the-great-resignation-tiet-lo-ve-van-hoa-lam-viec-sai-lam-350x250.jpeg" />
+        <Card.Img 
+          variant="top" 
+          src="https://haymora.com/blog/wp-content/uploads/2021/11/nhung-dieu-the-great-resignation-tiet-lo-ve-van-hoa-lam-viec-sai-lam-350x250.jpeg" />
         <Card.Body>
-          <Card.Title style={{fontSize: '20px', textAlign: 'initial', cursor: 'pointer', marginBottom: '10px'}}> 21 nghề dễ dẫn đến ly hôn ở Mỹ</Card.Title>
-          <Card.Subtitle style={{fontSize: '11px', textAlign: 'initial', marginBottom: '10px'}}>by <b>Minh Nguyen</b>,  25 November, 2021</Card.Subtitle>
-          <Card.Text style={{ textAlign: 'initial'}}>
-          Chúng ta đều hiểu việc giữ thái độ bình tĩnh, tích cực trong suốt quá trình phỏng vấn là điều ...
+
+          <Card.Title style={
+            {fontSize: '20px', 
+            textAlign: 'initial', 
+            cursor: 'pointer',
+            marginBottom: '10px'}}
+            > 21 nghề dễ dẫn đến ly hôn ở Mỹ
+          </Card.Title>
+          
+          <Card.Subtitle style={
+              {fontSize: '11px', 
+              textAlign: 'initial', 
+              marginBottom: '10px'}}
+              >by <b>Minh Nguyen</b>,  25 November, 2021
+          </Card.Subtitle>
+          
+          <Card.Text style={{ textAlign: 'initial'}}
+            >Chúng ta đều hiểu việc giữ thái độ bình tĩnh, tích cực trong suốt quá trình phỏng vấn là điều ...
           </Card.Text>
         </Card.Body>
       </Card>
