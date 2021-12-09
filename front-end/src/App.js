2546e82772f4a1d2f5371c791bda896b59140c75
huynguyen-jo-jo <73032228+huynguyen-jo-jo@users.noreply.github.com>
2021-12-09 12:19:07 +0700

    add desktop-7

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,14 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css';
 import Desktop3 from './Componant/Desktop3';
-
+import Desktop7 from './Componant/Desktop7';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route element={<Desktop3/>} path="/desktop-3"/>
-         
+          <Route element={<Desktop7/>} path="/desktop-7"/>
         </Routes>
       </BrowserRouter>
     </div>
