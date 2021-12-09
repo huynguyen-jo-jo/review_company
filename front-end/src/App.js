6ff46d7b7f302e3d64d7b5a8081efae79862c5e1
huynguyen-jo-jo <73032228+huynguyen-jo-jo@users.noreply.github.com>
2021-12-09 10:53:30 +0700

    add desktop 3

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,15 @@
 
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css';
-import Componant from './Componant/Text';
+import Desktop3 from './Componant/Desktop3';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route element={<Componant titile="desktop-1"/>} path="/desktop-1"/>
-          <Route element={<Componant titile="desktop-2"/>} path="/desktop-2"/>
-          <Route element={<Componant titile="desktop-3"/>} path="/desktop-3"/>
-          <Route element={<Componant titile="desktop-4"/>} path="/desktop-4"/>
-          <Route element={<Componant titile="desktop-5"/>} path="/desktop-5"/>
+          <Route element={<Desktop3/>} path="/desktop-3"/>
+         
         </Routes>
       </BrowserRouter>
     </div>
